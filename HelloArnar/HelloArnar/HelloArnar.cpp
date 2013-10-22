1ecfcc04e032e1f895dbd40f64e741da1c41c588
Gudmundur Arnar Astvaldsson <arnar@vivalditec.com>
2013-10-22 14:34:23 +0000

    Breytti lit i blue

--- HelloArnar/HelloArnar/HelloArnar.cpp
+++ HelloArnar/HelloArnar/HelloArnar.cpp
@@ -6,7 +6,7 @@ using namespace System;
 
 int main(array<System::String ^> ^args)
 {
-    Console::WriteLine(L"Hello World");
+    Console::WriteLine(L"Hello World. Now in NewBlue");
 
 	Console::WriteLine(L"Now what");
 
